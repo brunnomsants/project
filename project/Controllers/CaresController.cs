32a291158b345f1629a457102943ce2beec8268d
Brunno <brunnomurilo1@gmail.com>
2025-11-19 19:48:20 -0300

    Add project files.

---
+++ project/Controllers/CaresController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using project.Data;
+using project.Models;
+
+namespace project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CaresController : ControllerBase
+    {
+        private readonly projectContext _context;
+
+        public CaresController(projectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Cares
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cares>>> GetCares()
+        {
+            return await _context.Cares.ToListAsync();
+        }
+
+        // GET: api/Cares/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cares>> GetCares(int id)
+        {
+            var cares = await _context.Cares.FindAsync(id);
+
+            if (cares == null)
+            {
+                return NotFound();
+            }
+
+            return cares;
+        }
+
+        // PUT: api/Cares/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCares(int id, Cares cares)
+        {
+            if (id != cares.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cares).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CaresExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Cares
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Cares>> PostCares(Cares cares)
+        {
+            _context.Cares.Add(cares);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCares", new { id = cares.Id }, cares);
+        }
+
+        // DELETE: api/Cares/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCares(int id)
+        {
+            var cares = await _context.Cares.FindAsync(id);
+            if (cares == null)
+            {
+                return NotFound();
+            }
+
+            _context.Cares.Remove(cares);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CaresExists(int id)
+        {
+            return _context.Cares.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ project/Models/Cares.cs
@@ -0,0 +1,10 @@
+﻿namespace project.Models
+{
+    public class Cares
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Frequency { get; set; }
+    }
+}
